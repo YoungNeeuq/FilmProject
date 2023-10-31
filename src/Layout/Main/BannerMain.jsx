5cc8ccecb41d78eea994b553bc8e154b500b4572
YoungNeeuq <haulv.dev@gmail.com>
2023-10-31 08:36:18 +0700

    Update navigation main

--- src/Layout/Main/BannerMain.jsx
+++ src/Layout/Main/BannerMain.jsx
@@ -10,7 +10,7 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules"
 const BannerMain = () => {
   return (
     <div className="xl:mt-24 lg:mt-24 mt-14">
-      <Swiper
+      {/* <Swiper
         effect={"coverflow"}
         grabCursor={true}
         direction="horizontal"
@@ -89,7 +89,21 @@ const BannerMain = () => {
             <ion-icon name="arrow-forward-outline"></ion-icon>
           </div>
         </div>
-      </Swiper>
+      </Swiper> */}
+      <div className=" container mx-auto flex gap-10">
+        <div className="text-white">
+          <h2>Often searched</h2>
+          <div className="flex"></div>
+          <h2>Actors</h2>
+          <div className="flex"></div>
+        </div>
+        <div className=" flex gap-2  image-container relative xl:mx-20 mx-3">
+          <img className="z-10" src="../../assets/img/mainpage/1.png" alt="" />
+          <img className="z-10" src="../../assets/img/mainpage/2.png" alt="" />
+          <img className="z-10" src="../../assets/img/mainpage/3.png" alt="" />
+          <div className="absolute -inset-7 bg-gradient-to-r from-[#4c6aa7] to-[#772462] rounded-lg blur-xl opacity-95 xl:w-[100%] w-[100%] lg:w-[100%]  xl:h-[84%] lg:h-[84%] h-[75%] xl:top-[9%] top-[10%] left-0 mx-auto"></div>
+        </div>
+      </div>
     </div>
   )
 }
